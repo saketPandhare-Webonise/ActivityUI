a115f283ece1fa7bf8d8b7b494a3007aa69ec7f8
Saket.Webonise Pandhare <saket.webonisepandhare@SaketWebonises-iMac.local>
2018-02-01 15:52:27 +0530

    New project added

---
+++ BookMyShowUI/BookMyShowUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookMyShowUI
+//
+//  Created by Saket.Webonise Pandhare on 01/02/18.
+//  Copyright © 2018 Saket.Webonise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
